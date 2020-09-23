b19f97fced4fb1eec8cdb80a8a449b1a21f52e5c
ali <vefabirsemtadidegildir@gmail.com>
2020-09-23 14:26:44 +0200

    added the project which name is 001-roman-numerals-converter

--- python/coding-challenges/cc-003-find-the-largest-number/cc-003-find-the-largest-number.py.py
+++ python/coding-challenges/cc-003-find-the-largest-number/cc-003-find-the-largest-number.py.py
@@ -1,7 +1,13 @@
 NumberList = []
+
 for i in range(1, 6):
+
     print("Please write your", i, "st/nd number as an integer or float value to add list: ")
+
     number = float(input())
+
     NumberList.append(number)
+
 NumberList = sorted(NumberList)
+
 print("The largest number that you entered to list is:", NumberList[4])
